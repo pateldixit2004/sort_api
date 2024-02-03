72359cdf411c4e3a261a76b01caddf206331500f
dixit <patoliyadixit257@gamil.com>
2024-02-03 21:38:41 +0530

    tabBar

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:get/get.dart';
 import 'package:sort_api/screen/view/charts_screen.dart';
 import 'package:sort_api/screen/view/home_screen.dart';
 import 'package:sort_api/screen/view/other.dart';
+import 'package:sort_api/screen/view/tabScreen.dart';
 
 void main() {
   runApp(GetMaterialApp(debugShowCheckedModeBanner: false, routes: {
-    "/": (p0) => ChartsScreen(),
+    "/": (p0) => TabScreen(),
   }));
 }
