14e711ed1a3fe04ced5261d4b38499f39d9d3376
dixit <patoliyadixit257@gamil.com>
2024-02-05 21:10:02 +0530

    email_valofication

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,83 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:sort_api/screen/view/charts_screen.dart';
+// import 'package:sort_api/screen/view/home_screen.dart';
+// import 'package:sort_api/screen/view/other.dart';
+// import 'package:sort_api/screen/view/tabScreen.dart';
+//
+// void main() {
+//   runApp(GetMaterialApp(debugShowCheckedModeBanner: false, routes: {
+//     "/": (p0) => TabScreen(),
+//   }));
+// }
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:sort_api/screen/view/charts_screen.dart';
-import 'package:sort_api/screen/view/home_screen.dart';
-import 'package:sort_api/screen/view/other.dart';
-import 'package:sort_api/screen/view/tabScreen.dart';
 
 void main() {
-  runApp(GetMaterialApp(debugShowCheckedModeBanner: false, routes: {
-    "/": (p0) => TabScreen(),
-  }));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _emailController = TextEditingController();
+  final RegExp _emailRegExp = RegExp(
+    r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$',
+  );
+
+  bool _isEmailValid = true;
+
+  void _validateEmail() {
+    setState(() {
+      _isEmailValid = _emailRegExp.hasMatch(_emailController.text);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Email Validation Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Enter Email',
+                errorText: _isEmailValid ? null : 'Invalid email format',
+              ),
+              onChanged: (value) => _validateEmail(),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Do something with the valid email address
+                if (_isEmailValid) {
+                  print('Valid email: ${_emailController.text}');
+                } else {
+                  print('Invalid email');
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
--- lib/screen/view/tabScreen.dart
+++ lib/screen/view/tabScreen.dart
@@ -1,49 +1,47 @@
-import 'package:flutter/material.dart';
-
-class TabScreen extends StatefulWidget {
-  const TabScreen({super.key});
-
-  @override
-  State<TabScreen> createState() => _TabScreenState();
-}
-
-class _TabScreenState extends State<TabScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-        child: DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello world Dixit"),
-          bottom: const PreferredSize(
-            preferredSize: Size(double.infinity, 0),
-            child: TabBar(
-              labelPadding: EdgeInsets.only(right: 10),
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              isScrollable: true,
-              tabs: [
-                Tab(
-                  child: Text("First Screen is THis"),
-                ),
-                Tab(
-                  child: Text("Second Screen is THis"),
-                ),
-                Tab(
-                  child: Text("Third Screen is THis"),
-                ),
-              ],
-            ),
-          ),
-        ),
-        body: const TabBarView(
-          children: [
-            Text("First"),
-            Text("Second"),
-            Text("Third"),
-          ],
-        ),
-      ),
-    ));
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class TabScreen extends StatefulWidget {
+//   const TabScreen({super.key});
+//
+//   @override
+//   State<TabScreen> createState() => _TabScreenState();
+// }
+//
+// class _TabScreenState extends State<TabScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//         child: DefaultTabController(
+//       length: 3,
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text("Hello world Dixit"),
+//           bottom:  TabBar(
+//             tabAlignment: TabAlignment.center,
+//             // labelPadding: EdgeInsets.only(right: 10),
+//             // padding: EdgeInsets.symmetric(horizontal: 30),
+//             isScrollable: true,
+//             tabs: [
+//               Tab(
+//                 child: Text("First Screen is THis fyt fjy gjj ttu"),
+//               ),
+//               Tab(
+//                 child: Text("Second Screen is THis bhr tjy jyn "),
+//               ),
+//               Tab(
+//                 child: Text("Third Screen is THis t tjjy huu jy"),
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: const TabBarView(
+//           children: [
+//             Text("First"),
+//             Text("Second"),
+//             Text("Third"),
+//           ],
+//         ),
+//       ),
+//     ));
+//   }
+// }
